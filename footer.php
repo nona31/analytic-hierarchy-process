5a8aa926703b57f8834a9ce535c7ad3d8d43577e
Nona Shella <sellaasqueli@gmail.com>
2023-11-06 10:44:00 +0700

    changes

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </div> <!-- /wrapper -->
 
 <footer>
-	<p>MarselaNunesCopyright &copy; <?php echo date("Y"); ?></p>
+	<p>MuhammadFurqanCopyright &copy; <?php echo date("Y"); ?></p>
 </footer>
 
 <script src="js/jquery-3.2.1.js"></script>
